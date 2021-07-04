750752d3dca6eb0eade3217d70f136d0db33f432
Ruslan Petrovich <rkomehdaht@gmail.com>
2021-07-04 14:38:13 +0300

    Initial commit

---
+++ SQLHW.sql
@@ -0,0 +1,5 @@
+Create database UsersDB;
+Use UsersDB;
+CREATE TABLE UsersT (PersonID int,LastName varchar(30),FirstName varchar(30),Address varchar(100),City varchar(30));
+INSERT INTO UsersT 
+VALUES (1,'Petrovich','Ruslan','M.Bogdanovicha street, 48','Minsk');
